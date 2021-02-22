77d46a115e34ae58b07e4e77485328c7c1687489
jainayushee3 <Ayushee.Jain@lntinfotech.com>
2021-02-22 20:28:50 +0530

    First Commit

---
+++ src/app/imagegrid/imagegrid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagegrid',
+  templateUrl: './imagegrid.component.html',
+  styleUrls: ['./imagegrid.component.css']
+})
+export class ImagegridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
